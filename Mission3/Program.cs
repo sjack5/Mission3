fefac4c3e6461249f55ca573b03881ee2697ec05
Spencer Jacklin <sjacklin01@gmail.com>
2023-01-23 10:44:57 -0700

    Yay

--- Mission3/Program.cs
+++ Mission3/Program.cs
@@ -6,7 +6,27 @@ namespace Mission3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Tic-Tac-Toe with group 1-3!");
+
+            char[] board = new char[9];
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("Where would player 1 like to go? (Pick a number between 0-8)");
+                    int player1Choice = Convert.ToInt32(Console.ReadLine());
+
+                    board[player1Choice] = 'X';
+                }
+                else
+                {
+                    Console.WriteLine("Where would player 2 like to go? (Pick a number between 0-8)");
+                    int player2Choice = Convert.ToInt32(Console.ReadLine());
+
+                    board[player2Choice] = 'O';
+                }
+            }
         }
     }
 }
