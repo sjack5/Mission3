45f4a906f2249b00591f6bb0051f8d7f03d46211
Bronson Horne <bronsonhorne@gmail.com>
2023-01-25 14:22:57 -0700

    removed stuff

--- Mission3/supporting.cs
+++ Mission3/supporting.cs
@@ -34,30 +34,30 @@ namespace Mission3
                     if (arr[0] == arr[1] && arr[1] == arr[2])
                     {
                         return flag = 1;
-                        return ((char)arr[0]);
+                        return (arr[0]);
                     }
                     else if (arr[3] == arr[4] && arr[4] == arr[5])
                     {
                         return flag = 1; 
-                        return ((char)arr[3]);
+                        return (arr[3]);
 
                     }
                     else if (arr[6] == arr[7] && arr[7] == arr[8])
                     {
                         return flag = 1; 
-                        return ((char)arr[6]);
+                        return (arr[6]);
 
                     }
                     else if (arr[0] == arr[4] && arr[4] == arr[8])
                     {
                         return flag = 1; 
-                        return ((char)arr[0]);
+                        return (arr[0]);
 
                     }
                     else if (arr[2] == arr[4] && arr[4] == arr[6])
                     {
                         return flag = 1; 
-                        return ((char)arr[2]);
+                        return (arr[2]);
 
                     }
                     else
