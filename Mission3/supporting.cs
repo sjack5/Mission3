bf1229d9260422f8e23f7652f6bec23167d1a707
Bronson Horne <bronsonhorne@gmail.com>
2023-01-25 15:15:33 -0700

    updated winner

--- Mission3/supporting.cs
+++ Mission3/supporting.cs
@@ -24,44 +24,53 @@ namespace Mission3
         }
 
         //Contain a method that receives the game board array as input and returns if there is a winner and who it was
-        public string Winner(char[] arr)
+        public char Winner(char[] arr)
         {
-            int flag = 0;
-            while (flag != 1)
-            {
                 for (int i = 0; i <= 8; i++)
                 {
+                    //check rows
                     if (arr[0] == arr[1] && arr[1] == arr[2])
                     {
-                        return flag = 1;
                         return (arr[0]);
                     }
                     if (arr[3] == arr[4] && arr[4] == arr[5])
                     {
-                        return flag = 1; 
                         return (arr[3]);
 
                     }
                     if (arr[6] == arr[7] && arr[7] == arr[8])
                     {
-                        return flag = 1; 
                         return (arr[6]);
 
                     }
+                    //check diagonals
                     if (arr[0] == arr[4] && arr[4] == arr[8])
-                    {
-                        return flag = 1; 
+                    { 
                         return (arr[0]);
 
                     }
                     if (arr[2] == arr[4] && arr[4] == arr[6])
                     {
-                        return flag = 1; 
                         return (arr[2]);
 
                     }
-                    
-                 return "no winner yet";
+                    //check columns
+                    if (arr[0] == arr[3] && arr[3] == arr[6])
+                    {
+                        return (arr[0]);
+                    }
+                    if (arr[1] == arr[4] && arr[4] == arr[7])
+                    {
+                        return (arr[1]);
+
+                    }
+                    if (arr[2] == arr[5] && arr[5] == arr[8])
+                    {
+                        return (arr[2]);
+
+                    }
+
+                return "no winner yet";
                     
                 }
             }   
