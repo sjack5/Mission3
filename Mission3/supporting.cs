b8837f424a535598203ba2b0fcf8228ffec99038
Bronson Horne <bronsonhorne@gmail.com>
2023-01-25 15:24:23 -0700

    Char and tab issue

--- Mission3/supporting.cs
+++ Mission3/supporting.cs
@@ -26,53 +26,53 @@ namespace Mission3
         //Contain a method that receives the game board array as input and returns if there is a winner and who it was
         public char Winner(char[] arr)
         {
-                for (int i = 0; i <= 8; i++)
+            for (int i = 0; i <= 8; i++)
+            {
+                //check rows
+                if (arr[0] == arr[1] && arr[1] == arr[2])
                 {
-                    //check rows
-                    if (arr[0] == arr[1] && arr[1] == arr[2])
-                    {
-                        return (arr[0]);
-                    }
-                    if (arr[3] == arr[4] && arr[4] == arr[5])
-                    {
-                        return (arr[3]);
-
-                    }
-                    if (arr[6] == arr[7] && arr[7] == arr[8])
-                    {
-                        return (arr[6]);
-
-                    }
-                    //check diagonals
-                    if (arr[0] == arr[4] && arr[4] == arr[8])
-                    { 
-                        return (arr[0]);
-
-                    }
-                    if (arr[2] == arr[4] && arr[4] == arr[6])
-                    {
-                        return (arr[2]);
-
-                    }
-                    //check columns
-                    if (arr[0] == arr[3] && arr[3] == arr[6])
-                    {
-                        return (arr[0]);
-                    }
-                    if (arr[1] == arr[4] && arr[4] == arr[7])
-                    {
-                        return (arr[1]);
-
-                    }
-                    if (arr[2] == arr[5] && arr[5] == arr[8])
-                    {
-                        return (arr[2]);
-
-                    }
-
-                 return "no winner yet";
-                    
+                    return (arr[0]);
                 }
+                if (arr[3] == arr[4] && arr[4] == arr[5])
+                {
+                    return (arr[3]);
+
+                }
+                if (arr[6] == arr[7] && arr[7] == arr[8])
+                {
+                    return (arr[6]);
+
+                }
+                //check diagonals
+                if (arr[0] == arr[4] && arr[4] == arr[8])
+                { 
+                    return (arr[0]);
+
+                }
+                if (arr[2] == arr[4] && arr[4] == arr[6])
+                {
+                    return (arr[2]);
+
+                }
+                //check columns
+                if (arr[0] == arr[3] && arr[3] == arr[6])
+                {
+                    return (arr[0]);
+                }
+                if (arr[1] == arr[4] && arr[4] == arr[7])
+                {
+                    return (arr[1]);
+
+                }
+                if (arr[2] == arr[5] && arr[5] == arr[8])
+                {
+                    return (arr[2]);
+
+                }
+
+                return "N";
+                    
+            }
             
         }
     }
