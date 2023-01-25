56942cea59650f61d22e3494f882f6d9cbb19c96
Bronson Horne <bronsonhorne@gmail.com>
2023-01-25 16:32:39 -0700

    spacing

--- Mission3/supporting.cs
+++ Mission3/supporting.cs
@@ -9,21 +9,18 @@ namespace Mission3
     public class supporting
     {
         //Receive the “board” array from the driver class
-        //Contain a method that prints the board based on the information passed in
+        //Method that prints the board based on the information passed in
         public void BoardPrint(char[] arr)
         {
-            Console.WriteLine("|   |   |   |");
             Console.WriteLine("| {0} | {1} | {2} |", arr[0], arr[1], arr[2]);
-            Console.WriteLine("|___________|");
-            Console.WriteLine("|   |   |   |");
+            Console.WriteLine("|_____________|");
             Console.WriteLine("| {0} | {1} | {2} |", arr[3], arr[4], arr[5]);
-            Console.WriteLine("|___________|");
-            Console.WriteLine("|   |   |   |");
+            Console.WriteLine("|_____________|");
             Console.WriteLine("| {0} | {1} | {2} |", arr[6], arr[7], arr[8]);
 
         }
 
-        //Contain a method that receives the game board array as input and returns if there is a winner and who it was
+        //A method that receives the game board array as input and returns if there is a winner and who it was
         public char Winner(char[] arr)
         {
             //check rows
