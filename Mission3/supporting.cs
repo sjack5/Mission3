9f3e97c6a10cc7be542159b408f3e135de09643c
Bronson Horne <bronsonhorne@gmail.com>
2023-01-25 14:36:26 -0700

    b

--- Mission3/supporting.cs
+++ Mission3/supporting.cs
@@ -36,34 +36,33 @@ namespace Mission3
                         return flag = 1;
                         return (arr[0]);
                     }
-                    else if (arr[3] == arr[4] && arr[4] == arr[5])
+                    if (arr[3] == arr[4] && arr[4] == arr[5])
                     {
                         return flag = 1; 
                         return (arr[3]);
 
                     }
-                    else if (arr[6] == arr[7] && arr[7] == arr[8])
+                    if (arr[6] == arr[7] && arr[7] == arr[8])
                     {
                         return flag = 1; 
                         return (arr[6]);
 
                     }
-                    else if (arr[0] == arr[4] && arr[4] == arr[8])
+                    if (arr[0] == arr[4] && arr[4] == arr[8])
                     {
                         return flag = 1; 
                         return (arr[0]);
 
                     }
-                    else if (arr[2] == arr[4] && arr[4] == arr[6])
+                    if (arr[2] == arr[4] && arr[4] == arr[6])
                     {
                         return flag = 1; 
                         return (arr[2]);
 
                     }
-                    else
-                    {
-                        return flag = 0;
-                    }
+                    
+                 return "no winner yet";
+                    
                 }
             }   
         }
