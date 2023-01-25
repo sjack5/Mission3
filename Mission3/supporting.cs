2e931857f288c843f7f96fdce37c015c56dba6d1
Bronson Horne <bronsonhorne@gmail.com>
2023-01-25 16:34:21 -0700

    removed |

--- Mission3/supporting.cs
+++ Mission3/supporting.cs
@@ -13,9 +13,9 @@ namespace Mission3
         public void BoardPrint(char[] arr)
         {
             Console.WriteLine("| {0} | {1} | {2} |", arr[0], arr[1], arr[2]);
-            Console.WriteLine("|_____________|");
+            Console.WriteLine("_____________");
             Console.WriteLine("| {0} | {1} | {2} |", arr[3], arr[4], arr[5]);
-            Console.WriteLine("|_____________|");
+            Console.WriteLine("_____________");
             Console.WriteLine("| {0} | {1} | {2} |", arr[6], arr[7], arr[8]);
 
         }
